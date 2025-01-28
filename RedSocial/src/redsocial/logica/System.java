a8109ac5629490977ad182e1429ceadef3d1d899
ady1029 <adrianernestorodriguez2910@gmail.com>
2025-01-28 15:59:19 -0500

    actualizacion
    Co-Authored-By: samira698 <165163691+samira698@users.noreply.github.com>
    Co-Authored-By: Marlooon2004 <196682971+marlooon2004@users.noreply.github.com>

---
+++ RedSocial/src/redsocial/logica/System.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redsocial.logica;
+
+import cu.edu.cujae.ceis.graph.LinkedGraph;
+import cu.edu.cujae.ceis.graph.interfaces.ILinkedWeightedEdgeNotDirectedGraph;
+
+/**
+ *
+ * @author rodri
+ */
+public class System {
+    private String name;
+    private ILinkedWeightedEdgeNotDirectedGraph graph;
+
+    public System(String name, ILinkedWeightedEdgeNotDirectedGraph graph) {
+        this.name = name;
+        this.graph = new LinkedGraph();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ILinkedWeightedEdgeNotDirectedGraph getGraph() {
+        return graph;
+    }
+
+    public void setGraph(ILinkedWeightedEdgeNotDirectedGraph graph) {
+        this.graph = graph;
+    }
+   
+     
+    
+}
